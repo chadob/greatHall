2639dd77fa7b3d210c816e2f407e18166af86282
Chad Oehlschlaeger-Browne <chadnobrowne@gmail.com>
2019-03-10 19:01:24 -0700

    nmuch

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import reducer from "./reducers";
+import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router'; // react-router v4
+import { ConnectedRouter } from 'connected-react-router'
+import { store } from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById("root"));

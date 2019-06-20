2ef8add44654bcad0c6741319580cfe728804fc0
Chad Oehlschlaeger-Browne <chadnobrowne@gmail.com>
2019-06-19 23:17:10 -0700

    starting lighting

---
+++ src/components/ceiling.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../styles/hallway.css";
+import Radium from 'radium';
+import {Project} from "./project";
+
+class Ceiling extends React.Component {
+    render() {
+    }
+    return (
+      <div className="flats top" style={styles.top}>
+        <>
+      </div>
+    )
+}
+export default Radium(Ceiling);
